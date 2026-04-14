d27ba97592deb543990af367ceed2b10d8aeb805
BryanStrk <bryan_ed_8@hotmail.com>
2026-04-14 10:02:25 +0200

    feat: add ActivityController CRUD endpoints

---
+++ src/main/java/com/maspower/controller/ActivityController.java
@@ -0,0 +1,49 @@
+package com.maspower.controller;
+
+import com.maspower.model.Activity;
+import com.maspower.service.ActivityService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/activities")
+@RequiredArgsConstructor
+public class ActivityController {
+
+    private final ActivityService activityService;
+
+    @GetMapping
+    public ResponseEntity<List<Activity>> getAll() {
+        return ResponseEntity.ok(activityService.findAll());
+    }
+
+    @GetMapping("/future")
+    public ResponseEntity<List<Activity>> getFuture() {
+        return ResponseEntity.ok(activityService.findFutureActivities());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Activity> getById(@PathVariable Long id) {
+        return ResponseEntity.ok(activityService.findById(id));
+    }
+
+    @PostMapping
+    public ResponseEntity<Activity> create(@RequestBody Activity activity) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(activityService.save(activity));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Activity> update(@PathVariable Long id, @RequestBody Activity activity) {
+        return ResponseEntity.ok(activityService.update(id, activity));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        activityService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+}
